8ec5d170829600a410cdefa9302a3d287222cc0f
Wesley Garcia <wes@alltradestech.com>
2022-07-12 15:15:12 -0700

    Add functionality for Merchants#show; still need to get Postman test to pass

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index]
+      resources :merchants, only: %i[index show]
     end
   end
 end
